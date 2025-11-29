7c3dd650489f8a27ba0beb072eb3a96c77dd4019
StringWeaver <55291103+StringWeaver@users.noreply.github.com>
2025-11-29 23:35:13 +0800

    Initial Commit

---
+++ SwiftUIVideoPlayer/SwiftUIVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVideoPlayerApp.swift
+//  SwiftUIVideoPlayer
+//
+//  Created by StringWeaver on 2025/11/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
